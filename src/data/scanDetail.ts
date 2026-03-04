4e1029fd0239e3d795ffdd47c7b664b0ad3839f6
Shreyesh <shreyeshpachpor06@gmail.com>
2026-03-04 09:37:10 +0530

    Add scanDetail data structure for managing scan details

---
+++ src/data/scanDetail.ts
@@ -0,0 +1,19 @@
+export const scanDetail = {
+  id: "1",
+  type: "Grey Box",
+  target: "google.com",
+  startedAt: "Nov 22, 09:00AM",
+  credentials: "2 Active",
+  files: "Control.pdf",
+  checklists: "40/150",
+  progress: 45,
+  status: "In Progress",
+  steps: [
+    "Spidering",
+    "Mapping",
+    "Testing",
+    "Validating",
+    "Reporting",
+  ],
+  activeStep: 2,
+}
